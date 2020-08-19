c5b7fc6734ba5452cc493e458ff927f94ad58720
Omar Reda <omarredaelsayedmohamed@gmail.com>
2020-08-19 04:45:07 -0700

    finished_view_profile_connect_DB

--- User/User_Profile/user.py
+++ User/User_Profile/user.py
@@ -7,10 +7,33 @@ user_profile_bp = Blueprint('user_profile_bp', __name__, template_folder = 'temp
 
 @user_profile_bp.route('/View profile')
 def view_profile():
-    for user in allUsers:
-        if user.get_email() == session['email'] and user.get_password() == session['password']:
-            return render_template('User_Profile/ViewProfile.html',user = user)
+    user_email = session['email']
+    print("Session=", user_email)
+    user_password = session['password']
+    print("Session=", user_password)
 
+    sqlite_connection = sqlite3.connect('Mail_DB.db')
+    select_user_query = """SELECT ID, NAME, EMAIL, GENDER
+                            ,DATE_OF_BIRTH, CITY, COUNTRY, CONTACT
+                            , ACCOUNT_CREATION_DATE, ACTIVE FROM USER
+                            WHERE EMAIL = ? AND PASSWORD = ?;"""
+    select_user_query_data = (user_email, user_password)
+    user_data = sqlite_connection.execute(select_user_query, select_user_query_data)
+    user_record = {}
+    for data in user_data:
+        user_record['ID'] = data[0]
+        user_record['NAME'] = data[1]
+        user_record['EMAIL'] = data[2]
+        user_record['GENDER'] = data[3]
+        user_record['DATE_OF_BIRTH'] = data[4]
+        user_record['CITY'] = data[5]
+        user_record['COUNTRY'] = data[6]
+        user_record['CONTACT'] = data[7]
+        user_record['ACCOUNT_CREATION_DATE'] =  data[8]
+        user_record['ACTIVE'] = data[9]
+
+    sqlite_connection.close()
+    return render_template("User_Profile/ViewProfile.html", user = user_record)
 
 @user_profile_bp.route('/EditProfileForm', methods = ['POST','GET'])
 def set_new_attributes():
